5edbad5da39e5d7383cee2d32e27453137abe31a
Devix71 <nitronitro60@yahoo.com>
2020-09-10 16:37:46 +0300

    Implemented a recyclerview to display items from firebase.

--- app/src/main/java/com/example/harta/ui/home/HomeFragment.java
+++ app/src/main/java/com/example/harta/ui/home/HomeFragment.java
@@ -11,23 +11,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.SearchView;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ViewFlipper;
 
 import androidx.annotation.NonNull;
 import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.harta.Cafenea;
 import com.example.harta.R;
-import com.google.android.gms.common.api.GoogleApiClient;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.location.LocationSettingsRequest;
-import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
@@ -44,11 +46,13 @@ import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
 public class HomeFragment extends Fragment implements OnMapReadyCallback, View.OnClickListener {
+
     ViewFlipper viewFlipper;
     private static final String REQUESTING_LOCATION_UPDATES_KEY = "Update";
     public ArrayList<Marker> mrk;
@@ -60,78 +64,40 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
     private LocationCallback locationCallback;
     Boolean requestingLocationUpdates = true;
     DatabaseReference databaseCafea;
-
-    ValueEventListener valueEventListener = new ValueEventListener() {
-        @Override
-        public void onDataChange(DataSnapshot dataSnapshot) {
-            mrk.clear();
-            if (dataSnapshot.exists()) {
-                Log.e("Count ", "" + dataSnapshot.getChildrenCount());
-                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
-                    Cafenea cafenea = postSnapshot.getValue(Cafenea.class);
-                    assert cafenea != null;
-
-                    mrk.add(googleMap.addMarker(new MarkerOptions()
-                            .position(new LatLng(cafenea.getLatitude(), cafenea.getLongitude()))
-                            .title(cafenea.getName())
-                            .snippet(cafenea.getAddress())
-                            .icon(BitmapDescriptorFactory.defaultMarker
-                                    (BitmapDescriptorFactory.HUE_AZURE))));
+    private RecyclerView mResultList;
 
 
-                }
-            } else {
-                Log.e("ACCESARE ", "NU A MERS");
-            }
-        }
+    private void firebaseUserSearch(String searchText) {
+
+        Log.e("adapter", "A parcurs pana aici");
 
 
-        @Override
-        public void onCancelled(@NonNull DatabaseError error) {
-
-        }
-    };
-    SearchView searchView;
-    private long UPDATE_INTERVAL = 60000;  /* 60 secs */
-    private long FASTEST_INTERVAL = 5000; /* 5 secs */
-    private SettingsClient settingsClient;
-    private LocationSettingsRequest locationSettingsRequest;
-    private LocationRequest mLocationRequest;
-
-    private void fetchLastLocation() {
-        if (ActivityCompat.checkSelfPermission(HomeFragment.this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(HomeFragment.this.requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            requestPermissions(new String[]
-                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
-            return;
-        }
-
-        final Task<Location> task = fusedLocationProviderClient.getLastLocation();
-        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+        Query query = databaseCafea.orderByChild("name");
+        //.startAt(searchText).endAt(searchText + "\uf8ff");
+        FirebaseRecyclerOptions<Cafenea> options =
+                new FirebaseRecyclerOptions.Builder<Cafenea>()
+                        .setQuery(query, Cafenea.class)
+                        .setLifecycleOwner(this)
+                        .build();
+        FirebaseRecyclerAdapter<Cafenea, UserViewHolder> firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Cafenea, UserViewHolder>(options) {
             @Override
-            public void onSuccess(Location location) {
-                if (task.isSuccessful() && task.getResult() != null) {
-                    currentLocation = task.getResult();
-                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
-                            new LatLng(currentLocation.getLatitude(),
-                                    currentLocation.getLongitude()), 15));
-
-
-                }
+            protected void onBindViewHolder(@NonNull UserViewHolder holder, int position, @NonNull Cafenea model) {
+                holder.setDetails(model.getName(), model.getAddress());
             }
-        })
 
+            @NonNull
+            @Override
+            public UserViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.list_layout, parent, false);
 
-                .addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception e) {
-                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
-                        e.printStackTrace();
-                    }
-                });
-
+                return new UserViewHolder(view);
+            }
+        };
+        Log.e("adapter", "A parcurs pana aici");
+        mResultList.setAdapter(firebaseRecyclerAdapter);
+        firebaseRecyclerAdapter.startListening();
+        Log.e("adapter", "A parcurs TOT");
     }
-
-    private GoogleApiClient mGoogleApiClient;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -145,6 +111,10 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
         mMapView.onCreate(savedInstanceState);
 
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(HomeFragment.this.requireContext());
+
+        mResultList = (RecyclerView) view.findViewById(R.id.result_list);
+        mResultList.setHasFixedSize(true);
+        mResultList.setLayoutManager(new LinearLayoutManager(HomeFragment.this.getContext()));
 
         locationCallback = new LocationCallback() {
             @Override
@@ -182,6 +152,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
 
         searchView = view.findViewById(R.id.sv_location);
         updateValuesFromBundle(savedInstanceState);
+
         searchView.setQueryHint(" ¯_(ツ)_/¯ ");
         /*
         String [] columNames = { SearchManager.SUGGEST_COLUMN_TEXT_1 };
@@ -200,15 +171,18 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
             @Override
             public boolean onQueryTextSubmit(String query) {
+
                 String location = searchView.getQuery().toString();
-                for (int i = 0; i < mrk.size(); i++) {
+                firebaseUserSearch(location);
+               /* for (int i = 0; i < mrk.size(); i++) {
                     if (location.equals(mrk.get(i).getTitle())) {
-                        viewFlipper.showPrevious();
-                        googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mrk.get(i).getPosition(), 20));
-                        searchView.setQuery(null, false);
+
+                        //viewFlipper.showPrevious();
+                        //googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(mrk.get(i).getPosition(), 20));
+                       // searchView.setQuery(null, false);
                     }
 
-                }
+                }*/
                 return false;
             }
 
@@ -220,6 +194,75 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
 
         return view;
     }
+
+    ValueEventListener valueEventListener = new ValueEventListener() {
+        @Override
+        public void onDataChange(DataSnapshot dataSnapshot) {
+            mrk.clear();
+            if (dataSnapshot.exists()) {
+                Log.e("Count ", "" + dataSnapshot.getChildrenCount());
+                for (DataSnapshot postSnapshot : dataSnapshot.getChildren()) {
+                    Cafenea cafenea = postSnapshot.getValue(Cafenea.class);
+                    assert cafenea != null;
+
+                    mrk.add(googleMap.addMarker(new MarkerOptions()
+                            .position(new LatLng(cafenea.getLatitude(), cafenea.getLongitude()))
+                            .title(cafenea.getName())
+                            .snippet(cafenea.getAddress())
+                            .icon(BitmapDescriptorFactory.defaultMarker
+                                    (BitmapDescriptorFactory.HUE_AZURE))));
+
+
+                }
+            } else {
+                Log.e("ACCESARE ", "NU A MERS");
+            }
+        }
+
+
+        @Override
+        public void onCancelled(@NonNull DatabaseError error) {
+
+        }
+    };
+    SearchView searchView;
+    //private SettingsClient settingsClient;
+    //private LocationSettingsRequest locationSettingsRequest;
+
+    private void fetchLastLocation() {
+        if (ActivityCompat.checkSelfPermission(HomeFragment.this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(HomeFragment.this.requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]
+                    {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_CODE);
+            return;
+        }
+
+        final Task<Location> task = fusedLocationProviderClient.getLastLocation();
+        task.addOnSuccessListener(new OnSuccessListener<Location>() {
+            @Override
+            public void onSuccess(Location location) {
+                if (task.isSuccessful() && task.getResult() != null) {
+                    currentLocation = task.getResult();
+                    googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(
+                            new LatLng(currentLocation.getLatitude(),
+                                    currentLocation.getLongitude()), 15));
+
+
+                }
+            }
+        })
+
+
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("MapDemoActivity", "Error trying to get last GPS location");
+                        e.printStackTrace();
+                    }
+                });
+
+    }
+
+    //private GoogleApiClient mGoogleApiClient;
 
     /*
        @Override
@@ -243,6 +286,7 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
             case R.id.next: {
                 viewFlipper.setInAnimation(HomeFragment.this.requireContext(), android.R.anim.slide_in_left);
                 viewFlipper.setOutAnimation(HomeFragment.this.requireContext(), android.R.anim.slide_out_right);
+                searchView.setIconified(false);
                 viewFlipper.showNext();
                 break;
             }
@@ -250,9 +294,13 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
     }
 
     protected void startLocationUpdates() {
-        mLocationRequest = new LocationRequest();
+        LocationRequest mLocationRequest = new LocationRequest();
         mLocationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        /* 60 secs */
+        long UPDATE_INTERVAL = 60000;
         mLocationRequest.setInterval(UPDATE_INTERVAL);
+        /* 5 secs */
+        long FASTEST_INTERVAL = 5000;
         mLocationRequest.setFastestInterval(FASTEST_INTERVAL);
         if (ActivityCompat.checkSelfPermission(HomeFragment.this.requireContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(HomeFragment.this.requireContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             requestPermissions(new String[]
@@ -262,6 +310,27 @@ public class HomeFragment extends Fragment implements OnMapReadyCallback, View.O
         fusedLocationProviderClient.requestLocationUpdates(mLocationRequest, locationCallback, Looper.getMainLooper());
 
 
+    }
+
+    public static class UserViewHolder extends RecyclerView.ViewHolder {
+
+        View mView;
+
+        public UserViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+        }
+
+        public void setDetails(String userName, String address) {
+            TextView user_name = (TextView) mView.findViewById(R.id.nume_text);
+            TextView adress = (TextView) mView.findViewById(R.id.adresa_text);
+
+
+            user_name.setText(userName);
+            adress.setText(address);
+
+
+        }
     }
 
 
